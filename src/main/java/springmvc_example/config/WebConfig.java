2c0c8eea3bebfeb75ec18b6d536fd7a31931e0b3
mrazamat <azamat00189@mail.ru>
2019-01-08 15:08:51 +0500

    changed

---
+++ src/main/java/springmvc_example/config/WebConfig.java
@@ -0,0 +1,33 @@
+//https://www.jackrutorial.com/2018/01/creating-a-web-application-with-spring-4-mvc-example-for-user-signup-login-and-logout-with-password-encoder-using-eclipse-mysql-database.html
+package springmvc_example.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "springmvc_example" })
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(){
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
